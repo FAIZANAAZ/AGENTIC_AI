bdec91f99c3ee70f8069c01d1a675d5a5222c0df
FAIZANAAZ779 <faixanaaz@gmail.com>
2025-04-08 18:45:36 +0500

    sqlite

--- DATABSE/SQULITE/main.py
+++ DATABSE/SQULITE/main.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, text
 
 
 # Database file name
-DB_FILE = "my_squlite_db"
+DB_FILE = "my_squlite.db"
 # ye hm name de rhy hen data base ka ye bn kr bhi ayga ak file ki trha  lekin jb tb hm file ko run kreengy wahi main.py ko data  base connect kry ke bad  query likhny k bad
 
 # Create an SQLite engine connected to the real database
@@ -18,7 +18,7 @@ with engine.connect() as connection:
         
         #row sql 
         """
-        CREATE TABLE IF NOT EXISTS students (
+        CREATE TABLE IF NOT EXISTS students(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name VARCHAR(25) NOT NULL,
             age INTEGER
@@ -38,19 +38,23 @@ with engine.connect() as connection:
     ))
     
     # create add *************************
-    with engine.connect() as connection:
-        connection.execute(text(
-            """
-            INSERT INTO students
-            (name, age)
-            VALUES
-            ('Taha', 20),
-            ('Ali', 22),
-            ('Sara', 19);
-            """
-            # is trha hm jitny chahy otna insert kr skty hen 
-        ))
-        connection.commit()
+    # with engine.connect() as connection:
+    #     connection.execute(text(
+    #         """
+    #         INSERT INTO students
+    #         (name, age)
+    #         VALUES
+    #         ('Taha', 20),
+    #         ('Ali', 22),
+    #         ('Sara', 19);
+            
+    #         """
+    #         # is trha hm jitny chahy otna insert kr skty hen 
+    #     ))
+    #     connection.commit()
+    
+    # isko commint rkha he wrna wo bar bar add krta jayga
+    
     
     # hm inser krwa rhy hen data base me data ko student ke table me jo oper bnaya he name wahi ayga bhly change ho jay 
     # phir variable name phir kha VALUES phir oski value dedi hmny osky ander id hm nhi dengy kioky osko hmny autoincreament pr lagay howa he id wo khod generate krlega 
@@ -62,7 +66,10 @@ with engine.connect() as connection:
     #  .headers on
     # run .mode box 
     # in dono sy wo ak table ki chakal lelega box jesy 
-    # run .exit (ye exit ho jaye ga )
+    # run .exit (ye exit ho jaye ga yani database sy bahir a jaygy )
+    
+    # agr hm semi colomn nhi lgaty ya koi ghalti ho jay jisy terminal me koch bhi kro enter to ..> ayga to osy hm bahir nikalny ke liye (;)lga kr enter krygy
+    
     
     # run SELECT * FROM students WHERE id = 1; (ye id 1 ka student ka data dikhayga he )
     
@@ -77,6 +84,7 @@ with engine.connect() as connection:
         connection.commit()
         
         # YHA HMNY DELETE KRWA DIA HE STUDENT KO ID 2 KA
+        # jb bhi koi changging krty hen hmy connection.commit() krna lazmi he 
     
     #UPDATE data ***************
     with engine.connect() as connection:
