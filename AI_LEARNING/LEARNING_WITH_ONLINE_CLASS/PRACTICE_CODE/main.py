177e8ba9db05d4d5b56b8c820a794502f534c5e8
FAIZANAAZ779 <faixanaaz@gmail.com>
2025-05-08 18:16:17 +0500

    agebt coding

--- AI_LEARNING/LEARNING_WITH_ONLINE_CLASS/PRACTICE_CODE/main.py
+++ AI_LEARNING/LEARNING_WITH_ONLINE_CLASS/PRACTICE_CODE/main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from agents import Agent,Runner,OpenAIChatCompletionsModel,AsyncOpenAI,set_tracing_disabled
+from agents import Agent, RunConfig,Runner,OpenAIChatCompletionsModel,AsyncOpenAI,set_tracing_disabled
 
 load_dotenv()
 set_tracing_disabled(disabled=True)
@@ -34,3 +34,52 @@ def main():
   print(ans.final_output)
 main()  
 # ***********************************************************
+# ///////////////////////////////////////////////////////////
+
+# 2. RUN LEVEL
+
+gemini_api_key = "AIzaSyAbpBootC7bPNAQ8JsfbBWnpGfzOOzE7ak"
+
+#Reference: https://ai.google.dev/gemini-api/docs/openai
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+agent: Agent = Agent(name="Assistant", instructions="You are a helpful assistant")
+
+result = Runner.run_sync(agent, "Hello, how are you.", run_config=config)
+
+print(result.final_output)
+
+# ///////////////////////////////////////////////////////////
+# GLOBAL
+
+from agents import Agent, Runner, AsyncOpenAI, set_default_openai_client, set_tracing_disabled, set_default_openai_api
+
+gemini_api_key = "AIzaSyAbpBootC7bPNAQ8JsfbBWnpGfzOOzE7ak"
+set_tracing_disabled(True)
+set_default_openai_api("chat_completions")
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+set_default_openai_client(external_client)
+
+agent: Agent = Agent(name="Assistant", instructions="You are a helpful assistant", model="gemini-2.0-flash")
+
+result = Runner.run_sync(agent, "Hello")
+
+print(result.final_output)
